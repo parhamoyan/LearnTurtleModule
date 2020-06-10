f170712fda4996fe327dbff0993a44f114a3cf64
Parham Taheri | پرهام طاهری <62795984+parhamoyan@users.noreply.github.com>
2020-06-10 19:14:13 +0200

    Create EX13.py

---
+++ EX13.py
@@ -0,0 +1,18 @@
+from turtle import *
+setup(500, 500)
+speed(0)
+pensize(10
+color("black", "red")
+setheading(135)
+begin_fill()
+forward(100)
+penup()
+goto(0,0)
+pendown()
+setheading(45)
+forward(100)
+circle(41.5, 225)
+setheading(90)
+circle(41.5, 225)
+end_fill()
+hideturtle()
