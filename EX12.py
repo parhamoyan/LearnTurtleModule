199260341522b3fd0baeb935499bc071f7a40565
Parham Taheri | پرهام طاهری <62795984+parhamoyan@users.noreply.github.com>
2020-06-10 18:23:54 +0200

    Update EX12.py

--- EX12.py
+++ EX12.py
@@ -4,17 +4,17 @@ speed(0)
 pensize(1)
 
 for j in range(2):
-    for i in range(800//20+1):
+    for i in range(400//10+1):
         penup()
-        goto(-400*(-1)**j,200-i*10)
+        goto(-200*(-1)**j,200-i*10)
         pendown()
-        goto((-400+20*i)*(-1)**j,-200)
+        goto((-200+10*i)*(-1)**j,-200)
 
 for j in range(2):
-    for i in range(800//20+1):
+    for i in range(400//10+1):
         penup()
-        goto(-400*(-1)**j,-200+i*10)
+        goto(-200*(-1)**j,-200+i*10)
         pendown()
-        goto((-400+20*i)*(-1)**j,+200)
+        goto((-200+10*i)*(-1)**j,+200)
 
 hideturtle()
